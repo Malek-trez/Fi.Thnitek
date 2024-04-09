1819d048e9b45ccf78521d1bb6b45dcc81b246a1
MohamedKarrab <karrab2015@gmail.com>
2024-04-09 09:40:45 +0100

    added gitignore

---
+++ Client/src/components/login.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './login.css'; // Import your custom CSS file for additional styles
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+  const [error, setError] = useState(null);
+  const navigateTo = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(`http://localhost:8000/api/login`, formData, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (response.status >= 400) {
+        throw new Error(response.data.message || 'Login failed');
+      }
+
+      const data = response.data;
+      localStorage.setItem('token', data.token);
+      navigateTo('/');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <section>
+        <div className="container login-block">
+          <h2 className="text-center pb-3 mb-3 border-bottom border-primary">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">Username</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                className="form-control"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                className="form-control"
+                required
+              />
+            </div>
+            <button type="submit" className="button-5">Login</button>
+            {error && <p className="mt-3 text-danger">{error}</p>}
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Login;
