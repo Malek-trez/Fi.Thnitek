7aec1c26b1157430339eef9ff80254f008593371
MohamedKarrab <karrab2015@gmail.com>
2024-05-31 15:52:41 +0100

    fix bug

--- server/controllers/socketio/addFriend.js
+++ server/controllers/socketio/addFriend.js
@@ -5,48 +5,27 @@ const addFriend = async (socket, friendName, cb) => {
     cb({ done: false, errorMsg: "Cannot add self!" });
     return;
   }
-
   const friend = await redisClient.hgetall(`userId:${friendName}`);
   const currentFriendList = await redisClient.lrange(
-      `friends:${socket.user.username}`,
-      0,
-      -1
+    `friends:${socket.user.username}`,
+    0,
+    -1
   );
-  const friendFriendList = await redisClient.lrange(
-      `friends:${friendName}`,
-      0,
-      -1
-  );
-
   if (!friend.userId) {
     cb({ done: false, errorMsg: "User doesn't exist!" });
     return;
   }
   if (
-      currentFriendList &&
-      currentFriendList.indexOf(`${friendName}.${friend.userId}`) !== -1
+    currentFriendList &&
+    currentFriendList.indexOf(`${friendName}.${friend.userId}`) !== -1
   ) {
     cb({ done: false, errorMsg: "Connection already added!" });
     return;
   }
-  if (
-      friendFriendList &&
-      friendFriendList.indexOf(`${socket.user.username}.${socket.user.userId}`) !== -1
-  ) {
-    cb({ done: false, errorMsg: "Connection already added on the other side!" });
-    return;
-  }
 
-  // Add friend to current user's friend list
   await redisClient.lpush(
-      `friends:${socket.user.username}`,
-      [friendName, friend.userId].join(".")
-  );
-
-  // Add current user to friend's friend list
-  await redisClient.lpush(
-      `friends:${friendName}`,
-      [socket.user.username, socket.user.userId].join(".")
+    `friends:${socket.user.username}`,
+    [friendName, friend.userId].join(".")
   );
 
   const newFriend = {
